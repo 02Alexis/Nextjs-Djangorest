3f01ba6b0f12237b453effd3b5790f2ec981e69c
02Alexis <m_chael1012@hotmail.com>
2024-02-29 11:53:27 -0500

    capturando los datos del formulario

--- frontend/src/app/components/FormTask.jsx
+++ frontend/src/app/components/FormTask.jsx
@@ -1,12 +1,24 @@
+"use client";
+import { useState } from "react";
+
 function FormTask() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, description);
+  };
+
   return (
     <div className="bg-slate-200 p-7">
-      <form className="" action="">
+      <form className="" onSubmit={handleSubmit}>
         <h1 className="text-black fonr-bold">Añadir tarea</h1>
         <label htmlFor="title" className="text-xs text-black">
           Title:
         </label>
         <input
+          onChange={(e) => setTitle(e.target.value)}
           type="text"
           name="title"
           className="bg-slate-400 rounded-md p-2 mb-2 block w-full text-slate-900"
@@ -15,6 +27,7 @@ function FormTask() {
           Description:
         </label>
         <textarea
+          onChange={(e) => setDescription(e.target.value)}
           name="description"
           className="bg-slate-400 rounded-md p-2 mb-2 block w-full text-slate-900"
         ></textarea>
