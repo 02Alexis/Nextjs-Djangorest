db59a66b3f1d8cfc16a5273002f24dc5c99bfc5c
02Alexis <m_chael1012@hotmail.com>
2024-02-29 13:30:55 -0500

    marcar y demarcar tareas ya hechas

--- frontend/src/app/components/taskCard.jsx
+++ frontend/src/app/components/taskCard.jsx
@@ -18,13 +18,36 @@ function TaskCard({ task }) {
     }
   };
 
+  const handleDone = async (id) => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks/${id}/done/`,
+      {
+        method: "POST",
+      }
+    );
+    if (res.status === 200) {
+      router.refresh();
+    }
+  };
+
   return (
     <div className="bg-slate-500 px-4 py-3 mb-2 rounded-md flex justify-between items-center">
       <div>
-        <h2>{task.title}</h2>
+        <h2 className="font-bold">
+          {task.title} {task.done && <span>✅</span>}
+        </h2>
         <p>{task.description}</p>
       </div>
       <div className="flex justify-between gap-x-2">
+        <button
+          className={
+            "text-white rounded-md p-2 " +
+            (task.done ? "bg-gray-900" : "bg-yellow-500")
+          }
+          onClick={() => handleDone(task.id)}
+        >
+          {task.done ? "Desmarcar" : "Marcar"}
+        </button>
         <button
           className="bg-red-500 text-white rounded-md p-2"
           onClick={() => handleDelete(task.id)}
