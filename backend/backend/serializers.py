847e6226908640cfd48237af2b8578756116e2c7
02Alexis <m_chael1012@hotmail.com>
2024-02-29 10:42:34 -0500

    resolviendo fallos, se llama de forma erronea la ruta y no se importo de buena manera el modelo

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -3,5 +3,6 @@ from .models import Task
 
 class taskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Taskfields = ('id', 'title', 'description', 'done', 'created_at')
+        model = Task
+        fields = ('id', 'title', 'description', 'done', 'created_at')
         read_only_fields = ('id', 'created_at')
